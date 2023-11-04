ebd695b884ed235f59bc91d02aa3791b72e2ebe5
thully <thully.developer@gmail.com>
2023-11-04 15:14:28 +0200

    setup django as BE and react as FE

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-	path('', views.landing, name='landing'),
-	path('home', views.home, name='home'),
-	path('ajax/get_home_data', views.get_home_data, name='get_home_data'),
- ]
