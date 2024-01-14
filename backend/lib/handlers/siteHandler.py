966cda91e18b3fa5c713cca300d8977c87967beb
thullyDev <thully.developer@gmail.com>
2024-01-14 14:57:56 +0200

    did the get_default_site_data, and et_default_site_data

--- backend/lib/handlers/siteHandler.py
+++ backend/lib/handlers/siteHandler.py
@@ -26,3 +26,96 @@ class SiteHandler:
 
         site_data = self.get_site_data()
         return len(site_data.get(amount_type, ""))
+
+
+    # def reset(self)
+    
+    def set_default_site_data(self):
+        site_data = self.get_default_site_data()
+        database.hset(name="site_data", data=site_data, expiry=False)
+        return site_data
+
+    def get_default_site_data(self):
+        return {
+            "scripts": {
+                "head_scripts": [
+                    {"name": "global_head", "value": ""},
+                    {"name": "home_head", "value": ""},
+                    {"name": "landing_head", "value": ""},
+                    {"name": "filter_head", "value": ""},
+                    {"name": "profile_head", "value": ""},
+                    {"name": "anime_head", "value": ""},
+                    {"name": "watch_head", "value": ""},
+                    {"name": "watch_together_browsing_head", "value": ""},
+                    {"name": "watch_together_anime_head", "value": ""},
+                ],
+                "foot_scripts": [
+                    {"name": "global_foot", "value": ""},
+                    {"name": "home_foot", "value": ""},
+                    {"name": "landing_foot", "value": ""},
+                    {"name": "filter_foot", "value": ""},
+                    {"name": "profile_foot", "value": ""},
+                    {"name": "anime_foot", "value": ""},
+                    {"name": "watch_foot", "value": ""},
+                    {"name": "watch_together_browsing_foot", "value": ""},
+                    {"name": "watch_together_anime_foot", "value": ""},
+                ],
+                "ads_scripts": {
+                    "global": [
+                        {"name": "top_advertisement", "value": "", "height": ""},
+                        {"name": "bottom_advertisement", "value": "", "height": ""},
+                    ],
+                    "landing": [
+                        {"name": "middle_advertisement", "value": "", "height": ""},
+                    ],
+                    "watch": [
+                        {"name": "under_player_advertisement", "value": "", "height": ""},
+                        {"name": "under_suggestions_advertisement", "value": "", "height": ""},
+                    ],
+                },
+            },
+            "values": {
+                "images": [
+                    {"key": "site_logo", "value": "/static/images/site-logo.png"},
+                    {"key": "favicon_logo", "value": "/static/images/favicon.png"},
+                    {"key": "alert", "value": "/static/images/gifs/alert.gif"},
+                    {"key": "maintenance", "value": "/static/images/gifs/maintenance.gif"},
+                    {"key": "empty", "value": "/static/images/gifs/empty.gif"},
+                ],
+                "inputs": [
+                    {"value": "AnimeHoshi", "key": "site_name"},
+                    {"value": "admin@animehoshi.com", "key": "email"},
+                    {"value": "Watch Anime On AnimeHoshi For No ads | AnimeHoshi", "key": "title"},
+                    {
+                        "value": "AnimeHoshi is a vibrant online platform offering a diverse collection of anime content for free streaming. With an extensive library spanning genres and popular titles, AnimeHoshi provides enthusiasts with an immersive experience. User-friendly navigation and high-quality playback make it a go-to destination for anime lovers seeking free, accessible entertainment.",
+                        "key": "site_description",
+                        "type": "field",
+                    },
+                ],
+                "socials": [
+                    {"value": "https://discord.com/", "key": "discord"},
+                    {"value": "https://twitter.com/", "key": "twitter"},
+                    {"value": "https://reddit.com/", "key": "reddit"},
+                    {"value": "https://ko-fi.com/", "key": "donate"},
+                ],
+            }
+            "settings": [
+                {"key": "maintanence", "value": False},
+                {"key": "adblocker_detection", "value": True},
+                {"key": "alert", "value": True},
+                {"key": "authentication", "value": True},
+                {"key": "anime", "value": True},
+                {"key": "watch", "value": True},
+                {"key": "watch_togather", "value": True},
+                {"key": "user", "value": True},
+                {"key": "schedule", "value": True},
+                {"key": "features", "value": True},
+                {"key": "footer", "value": True},
+                {"key": "landing", "value": True},
+                {"key": "donation", "value": True},
+                {"key": "socials", "value": True},
+                {"key": "contact", "value": True},
+                {"key": "dark_mode", "value": True},
+            ]
+        }
+
--- backend/lib/routers/admin.py
+++ backend/lib/routers/admin.py
@@ -58,44 +58,6 @@ class Admin(Base):
 
     @adminValidator
     def scripts(self, request, context):
-        head_scripts = [
-            {"name": "global_head", "value": ""},
-            {"name": "home_head", "value": ""},
-            {"name": "landing_head", "value": ""},
-            {"name": "filter_head", "value": ""},
-            {"name": "profile_head", "value": ""},
-            {"name": "anime_head", "value": ""},
-            {"name": "watch_head", "value": ""},
-            {"name": "watch_together_browsing_head", "value": ""},
-            {"name": "watch_together_anime_head", "value": ""},
-        ]
-
-        foot_scripts = [
-            {"name": "global_foot", "value": ""},
-            {"name": "home_foot", "value": ""},
-            {"name": "landing_foot", "value": ""},
-            {"name": "filter_foot", "value": ""},
-            {"name": "profile_foot", "value": ""},
-            {"name": "anime_foot", "value": ""},
-            {"name": "watch_foot", "value": ""},
-            {"name": "watch_together_browsing_foot", "value": ""},
-            {"name": "watch_together_anime_foot", "value": ""},
-        ]
-
-        ads_scripts = {
-            "global": [
-                {"name": "top_advertisement", "value": "", "height": ""},
-                {"name": "bottom_advertisement", "value": "", "height": ""},
-            ],
-            "landing": [
-                {"name": "middle_advertisement", "value": "", "height": ""},
-            ],
-            "watch": [
-                {"name": "under_player_advertisement", "value": "", "height": ""},
-                {"name": "under_suggestions_advertisement", "value": "", "height": ""},
-            ],
-        }
-
         set_context(context=context, data={
             "head_scripts": head_scripts,
             "foot_scripts": foot_scripts,
@@ -105,30 +67,7 @@ class Admin(Base):
 
     @adminValidator
     def general(self, request, context):
-        images = [
-            {"key": "site_logo", "value": "/static/images/site-logo.png"},
-            {"key": "favicon_logo", "value": "/static/images/favicon.png"},
-            {"key": "alert", "value": "/static/images/gifs/alert.gif"},
-            {"key": "maintenance", "value": "/static/images/gifs/maintenance.gif"},
-            {"key": "empty", "value": "/static/images/gifs/empty.gif"},
-        ]
-        inputs = [
-            {"value": "AnimeHoshi", "key": "site_name"},
-            {"value": "admin@animehoshi.com", "key": "email"},
-            {"value": "Watch Anime On AnimeHoshi For No ads | AnimeHoshi", "key": "title"},
-            {
-                "value": "AnimeHoshi is a vibrant online platform offering a diverse collection of anime content for free streaming. With an extensive library spanning genres and popular titles, AnimeHoshi provides enthusiasts with an immersive experience. User-friendly navigation and high-quality playback make it a go-to destination for anime lovers seeking free, accessible entertainment.",
-                "key": "site_description",
-                "type": "field",
-            },
-        ]
-
-        socials = [
-            {"value": "https://discord.com/", "key": "discord"},
-            {"value": "https://twitter.com/", "key": "twitter"},
-            {"value": "https://reddit.com/", "key": "reddit"},
-            {"value": "https://ko-fi.com/", "key": "donate"},
-        ]
+        # values
 
         set_context(context=context, data={
             "images": images,
@@ -139,24 +78,7 @@ class Admin(Base):
 
     @adminValidator
     def advance(self, request, context):
-        settings = [
-            {"key": "maintanence", "value": False},
-            {"key": "adblocker_detection", "value": True},
-            {"key": "alert", "value": True},
-            {"key": "authentication", "value": True},
-            {"key": "anime", "value": True},
-            {"key": "watch", "value": True},
-            {"key": "watch_togather", "value": True},
-            {"key": "user", "value": True},
-            {"key": "schedule", "value": True},
-            {"key": "features", "value": True},
-            {"key": "footer", "value": True},
-            {"key": "landing", "value": True},
-            {"key": "donation", "value": True},
-            {"key": "socials", "value": True},
-            {"key": "contact", "value": True},
-            {"key": "dark_mode", "value": True},
-        ]
+        settings = 
 
         set_context(context=context, data={
             "settings": settings,
@@ -165,157 +87,8 @@ class Admin(Base):
 
     @adminValidator
     def admins(self, request, context):
-        admins_items = [
-                {
-                    'id': 0,
-                    'username': 'animeGirl',
-                    'email': 'animegirl@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/564x/07/d4/34/07d434bcb00e39c8e8d25df1cc89a333.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 1,
-                    'username': 'animeBoy',
-                    'email': 'animeBoy@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/ef/e9/73/efe97322d26afdbb85e03c52b1db7c10.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 2,
-                    'username': 'Megumi',
-                    'email': 'megumi@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/736x/02/05/28/020528711a47abd638ed5ee670cc4705.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 3,
-                    'username': 'Gojo',
-                    'email': 'gojo@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/736x/50/c4/bd/50c4bdba9bbe22a46733edbdb55b65a2.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 4,
-                    'username': 'Kenji',
-                    'email': 'kenji@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/85/42/bb/8542bb42f5e7369e953049fa14ba5170.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 5,
-                    'username': 'sky',
-                    'email': 'sky@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/1d/da/d3/1ddad3615c85b90dccd31c2b5fbcb5a1.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 6,
-                    'username': 'vi',
-                    'email': 'viCatlin@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/e5/60/0e/e5600eac05e07ae2e74492d5060130f0.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 7,
-                    'username': 'jayce',
-                    'email': 'jayce@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/61/b9/59/61b9595898d45dd9e20261a5864455c6.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 8,
-                    'username': 'jinx',
-                    'email': 'jinx@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/7b/48/e1/7b48e1b4561ab7962b582ca20f78e914.jpg',
-                    'deleted': False,
-                },
-                {
-                    'id': 9,
-                    'username': 'nightGirl',
-                    'email': 'nightGirl@gmail.com',
-                    'profile_image': 'https://i.pinimg.com/236x/79/94/e7/7994e7bfaa011c4c4d0675e09cea4d3a.jpg',
-                    'deleted': False,
-                },
-            ]
-
         set_context(context=context, data={
             "admins_items": admins_items,
             "admins_count": len(admins_items),
         })
         return self.root(request=request, context=context, template="pages/admin/admins.html")   
-
-
-
-    # @adminValidator
-    # def get_scripts(self, request):
-    #     user = self.GET_CREDITIALS(DATA=request.COOKIES, user_type="admin")
-
-    #     if not user:
-    #         return self.forbidden_response(data={"message": "login"})
-
-    #     site_data = self.get_site_data()
-    #     scripts = site_data.get("scripts")
-
-    #     return self.successful_response(data={"data": scripts})
-
-    # @adminValidator
-    # def get_attributes(self, request):
-    #     user = self.GET_CREDITIALS(DATA=request.COOKIES, user_type="admin")
-
-    #     if not user:
-    #         return self.forbidden_response(data={"message": "login"})
-
-    #     site_data = self.get_site_data()
-    #     attributes = site_data.get("attributes")
-
-    #     return self.successful_response(data={"data": attributes})
-
-    # @adminValidator
-    # def get_values(self, request):
-    #     user = self.GET_CREDITIALS(DATA=request.COOKIES, user_type="admin")
-
-    #     if not user:
-    #         return self.forbidden_response(data={"message": "login"})
-
-    #     site_data = self.get_site_data()
-    #     values = site_data.get("values")
-
-    #     return self.successful_response(data={"data": values})
-
-    # @adminValidator
-    # def get_settings(self, request):
-    #     user = self.GET_CREDITIALS(DATA=request.COOKIES, user_type="admin")
-
-    #     if not user:
-    #         return self.forbidden_response(data={"message": "login"})
-
-    #     site_data = self.get_site_data()
-    #     settings = site_data.get("settings")
-
-    #     return self.successful_response(data={"data": settings})
-
-
-    # @adminValidator
-    # def dashboard(self, request):
-    #     user = self.GET_CREDITIALS(request.COOKIES)
-
-    #     if not user:
-    #         return redirect("/")
-
-    #     site_data = self.get_site_data()
-    #     scripts_amount = len(site_data.get("scripts", {}))
-    #     values_amount = len(site_data.get("values", {}))
-    #     attributes_amount = len(site_data.get("attributes", {}))
-    #     settings_amount = len(site_data.get("settings", {}))
-    #     users_amount = len(users)  # top 10 latest users
-    #     data = {
-    #         "users_amount": users_amount,
-    #         "scripts_amount": scripts_amount,
-    #         "values_amount": values_amount,
-    #         "attributes_amount": attributes_amount,
-    #         "settings_amount": settings_amount,
-    #         "users": users,
-    #     }
-
-    #     return self.successful_response(data={"data": data})
-
