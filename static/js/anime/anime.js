061fb84bd14517ae676ef6e6c548ac1035737f7e
thullyDev <thully.developer@gmail.com>
2024-02-01 17:09:41 +0200

    did the search

--- static/js/anime/anime.js
+++ static/js/anime/anime.js
@@ -1,3 +1,17 @@
 (function () {
+	const searchInp = $(".search-input")
 	$(".menu-btn").click(() => showCloseEle(".mobile-viewer-con", "slide"));
+	$(".search-btn").click(() => search())
+	searchInp.change(() => search())
+
+	function search() {
+		const value = searchInp.val()
+		const type = searchInp.data("type")
+
+		if (value) return 
+
+		const url = type == "latino" ? "/latino/search/" : "/main/filter/"
+		const query = `${url}?keywords=${encodeURIComponent(originalString)}`
+		window.location.href = query 
+	}
 })();
