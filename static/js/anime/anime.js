1f8864d1010bccc3532e0bbad7351324f350277e
thullyDev <thully.developer@gmail.com>
2024-02-19 14:06:41 +0200

    did the frontend for the make_watch_room, no testing tho

--- static/js/anime/anime.js
+++ static/js/anime/anime.js
@@ -1,17 +1,68 @@
 (function () {
 	const searchInp = $(".search-input")
 	$(".menu-btn").click(() => showCloseEle(".mobile-viewer-con", "slide"));
+	$(".watch2gather-btn, .watch-room-modal-close-btn").click(() => showCloseEle(".watch-room-modal-con", "fade"));
+	$(".create-room-bt").click(() => createRoom())
+
 	$(".search-btn").click(() => search())
+
 	searchInp.change(() => search())
-
-	function search() {
-		const value = searchInp.val()
-		const type = searchInp.data("type")
-
-		if (value) return 
-
-		const url = type == "latino" ? "/latino/search/" : "/main/filter/"
-		const query = `${url}?keywords=${encodeURIComponent(value)}`
-		window.location.href = query 
-	}
 })();
+
+function search() {
+	const value = searchInp.val()
+	const type = searchInp.data("type")
+
+	if (value) return 
+
+	const url = type == "latino" ? "/latino/search/" : "/main/filter/"
+	const query = `${url}?keywords=${encodeURIComponent(value)}`
+	window.location.href = query 
+}
+
+function getRoomInputs() {
+  const inputs = $(".watch-room-input")
+  const data = {}
+
+  inputs.each((_, ele) => {
+    const thisEle = $(ele)
+    const name = thisEle.data("name")
+    const value = thisEle.is(':checked') : $.trim(thisEle.data("value")) || $.trim(thisEle.val())  
+
+    data[name] = value
+  })
+
+  return data
+}
+
+function createRoom() {
+	data = getRoomInputs()
+
+	if (!data.room_name) {
+		showAlert({ message: "type the room name"})
+		return
+	} 
+
+	data.csrfmiddlewaretoken = csrfToken
+
+	$.ajax({
+	    data,
+	    url: "/user/ajax/post/make_watch_room/",
+	    type: 'POST',
+	    beforeSend: function() {
+	    	showLoader()
+	    },
+	    success: (response) => {
+	        const { message, room_id } = response
+			showAlert({ message })
+			closeLoader()
+			window.location.replace(`/watch2gather/${room_id}`)
+	    },
+	    error: (error) => {
+	    	const { message } = error.responseJSON
+			showAlert({ message })
+			closeLoader()
+    	}	
+	});
+
+}
