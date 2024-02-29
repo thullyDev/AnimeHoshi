8a807aca6b6c7f9e347ab21b9a61d9250e40c042
thullyDev <thully.developer@gmail.com>
2024-02-29 08:50:08 +0200

    made it only if the settings.debug is true that room_sweeper threading should run, for development purposes

--- backend/lib/handlers/liveChatHandler.py
+++ backend/lib/handlers/liveChatHandler.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from .apiHandler import ApiHandler
 from ..database import Database
 from ..resources import LIVECHAT_API_URL as live_base, CRASH, SUCCESSFUL, get_time_difference
-from pprint import pprint
 import datetime
 import threading
 
@@ -14,11 +14,9 @@ class LiveChat(ApiHandler):
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super().__new__(cls)
-            # cls._instance.start_room_sweeper()
+            cls._instance.start_room_sweeper()
         return cls._instance
 
-    def __init__(self):
-        self.start_room_sweeper()
 
     def create_room(self):
         data = self.request(base=live_base, endpoint=self.create_room_endpoint, https_safe=False)
@@ -34,7 +32,11 @@ class LiveChat(ApiHandler):
         return data
 
     def room_sweeper(self):
+        print("ROOM_SWEEPER running")
         rooms = database.get_all(unit="rooms")
+
+        if not len(rooms): return 
+
         current_time = datetime.datetime.now(datetime.timezone.utc)
 
         for room in rooms:
@@ -46,7 +48,8 @@ class LiveChat(ApiHandler):
 
             if time_difference.total_seconds() < 24 * 3600: return # 3600 = 24 hours
 
-            # self.delete_room(room)
+            self.delete_room(room)
+
 
     def delete_room(self, room): # 3600 = 24 hours
         database.delete(unit="rooms", unique_id=room["room_id"], key="room_id")
@@ -59,7 +62,12 @@ class LiveChat(ApiHandler):
             self._room_sweeper_timer.cancel()
 
     def _room_sweeper_loop(self):
-        self.room_sweeper()  # Run the room sweeper function
+        self.room_sweeper()  
+
+        # this is here to ensure we have a good development 
+        # enviroment without the threading slowing the programme down
+        if settings.DEBUG: return 
+
         # Schedule the next execution
-        self._room_sweeper_timer = threading.Timer(3600, self._room_sweeper_loop) # 3600 = 24 hours
+        self._room_sweeper_timer = threading.Timer(10, self._room_sweeper_loop) # 3600 = 24 hours
         self._room_sweeper_timer.start()
